ccf3acf612a0bb29d99b69dd6bd9abc1ce6bab79
EldritchBear <>
2022-04-10 22:59:22 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -57,7 +57,7 @@ void enlarge(HashMap * map) {
   
   Pair **old_buckets[map->size];
 
-  old_buckets = buckets;
+  old_buckets = buckets->buckets;
   
   map->capacity *= 2;
   map->buckets = (Pair **) calloc (map->capacity,sizeof(Pair *));
