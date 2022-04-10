aab14a0b5d1c37209d07299254c73b27697c06b8
EldritchBear <>
2022-04-10 21:11:30 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -74,8 +74,11 @@ void eraseMap(HashMap * map,  char * key) {
 }
 
 Pair * searchMap(HashMap * map,  char * key) {   
-
-
+  long idx = hash(key,map->capacity);
+  while ((map->buckets[idx] != NULL) && (map->buckets[idx]->key != NULL)){
+    if(is_equal(key,map->buckets[idx]->key)==1)return;
+    idx = ((idx+1)%map->capacity);
+  }
     return NULL;
 }
 
