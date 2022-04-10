c5115fc1f545307375df0e33d73be8dc370ab0d5
EldritchBear <>
2022-04-10 22:33:53 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -56,7 +56,7 @@ void enlarge(HashMap * map) {
     enlarge_called = 1; //no borrar (testing purposes)
   Pair** a = map->buckets;
   map->capacity *= 2;
-  map->buckets = createMap(map->capacity);
+  map = createMap(map->capacity);
   map->size = 0;
   insertmap(map, a->key, a->value);
 }
