02516e7b4f76992f3e4105ffab2416e7716bf1d6
EldritchBear <>
2022-04-10 21:12:21 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -76,7 +76,7 @@ void eraseMap(HashMap * map,  char * key) {
 Pair * searchMap(HashMap * map,  char * key) {   
   long idx = hash(key,map->capacity);
   while ((map->buckets[idx] != NULL) && (map->buckets[idx]->key != NULL)){
-    if(is_equal(key,map->buckets[idx]->key)==1)return p;
+    if(is_equal(key,map->buckets[idx]->key)==1)return idx;
     idx = ((idx+1)%map->capacity);
   }
     return NULL;
